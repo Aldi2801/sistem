3fcdf5366f05cb321716156302f989d6afa90973
rixkyduwi <55983435+rixkyduwi@users.noreply.github.com>
2023-12-05 12:39:48 +0700

    add logout

--- app/login.py
+++ app/login.py
@@ -1,5 +1,5 @@
 from . import app,db,bcrypt,user_datastore,security,jwt,Role,User
-from flask import request,render_template,redirect,url_for
+from flask import request,render_template,redirect,url_for,jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
     
 @app.route('/masuk')
@@ -24,12 +24,12 @@ def login():
             return redirect(url_for('masuk',error="Invalid password"))
 
 # Endpoint yang memerlukan autentikasi
-@app.route('/protected', methods=['GET'])
+@app.route('/logout')
 @jwt_required()
-def protected():
+def logout():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
+    return redirect(url_for('masuk', msg='logout sukses'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -51,6 +51,10 @@ def register():
         user = user_datastore.create_user(username=username, password=hashed_password, active=True)
         db.session.commit()
 
-        return redirect(url_for('login', msg='Registration Successful'))
+        return redirect(url_for('masuk', msg='Registration Successful'))
 
     return render_template('admin/register.html')
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return redirect(url_for('login'))
