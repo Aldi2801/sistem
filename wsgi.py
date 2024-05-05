1182d51efee27fbd8cbaeabdb9f076b57a9d7283
Wowos27 <149362978+Wowos27@users.noreply.github.com>
2024-05-05 11:39:22 +0700

    wmdajmw
    Co-Authored-By: Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>

---
+++ wsgi.py
@@ -0,0 +1,18 @@
+from app import app,db
+from flask import request, jsonify
+import datetime
+#ssl_context='adhoc'
+@app.route('/chatbot/get')
+def chatbot():
+  userText = request.args.get('msg')
+  tanggal = datetime.datetime.now()
+  tanggal_baru = tanggal.strftime('%Y-%m-%d %H:%M:%S')
+  import chatbot
+  respon = [chatbot.chatbot_response(userText)]
+  print(respon)
+  return respon
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run()
