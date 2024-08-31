7082daa275f7499ad9a7ea25c56e8975526aedca
Wowos27 <149362978+Wowos27@users.noreply.github.com>
2024-08-31 11:19:44 +0700

    update
    Co-Authored-By: Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from app import app,db
 #ssl_context='adhoc'
-
+from flask import request, jsonify
+import datetime
+#ssl_context='adhoc'
+@app.route('/chatbot/get')
+def chatbot():
+  userText = request.args.get('msg')
+  tanggal = datetime.datetime.now()
+  tanggal_baru = tanggal.strftime('%Y-%m-%d %H:%M:%S')
+  import chatbot
+  respon = chatbot.generate_response(userText)
+  print(respon)
+  return respon
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
